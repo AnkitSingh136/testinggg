42a68bbc2b15ce990779854b844e668de6a6e0bf
Ankit singh <125378145+Ankitcoder136@users.noreply.github.com>
2025-04-14 08:31:35 +0530

    Connected to Railway MySQL using env vars

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,10 +4,10 @@ dotenv.config();
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'aannkkiitt136',
-  database: process.env.MYSQL_DATABASE || 'ace_aptitude',
+  host: process.env.MYSQL_HOST ,//
+  user: process.env.MYSQL_USER,//
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
